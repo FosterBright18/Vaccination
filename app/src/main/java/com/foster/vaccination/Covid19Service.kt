f3d1473e010be09eaa8593415b3879ec96f448b6
FosterBright18 <FosterBright18@gmail.com>
2022-02-01 10:08:08 -0800

    Fix it future foster

---
+++ app/src/main/java/com/foster/vaccination/Covid19Service.kt
@@ -0,0 +1,10 @@
+package com.foster.vaccination
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Covid19Service {
+    @GET("vaccine/coverage/countries")
+    fun getVaccinations(@Query("lastdays") lastDays : Int) : Call<List<Vaccination>>
+}
--- app/src/main/java/com/foster/vaccination/VaccinationListActivity.java
+++ app/src/main/java/com/foster/vaccination/VaccinationListActivity.java
@@ -2,4 +2,12 @@ package com.foster.vaccination;
 
 public class VaccinationListActivity {
 
+    val vaccineApi = RetrofitHelper.getInstance().create(Covid19Service::class.java)
+    val vaccineCall = vaccineApi.getVaccinations(10)
+
+    vaccineCall.enueue(object : Callbacl<List<VaccinationInfo>> {
+
+    }
+    )
+
 }
